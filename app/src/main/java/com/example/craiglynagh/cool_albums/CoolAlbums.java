5ad860ae69128b0189e5f42466b6db484adc8587
Craig Lynagh <craiglynagh@gmail.com>
2017-08-09 21:08:58 +0100

    getListTest returns successfully

--- app/src/main/java/com/example/craiglynagh/cool_albums/CoolAlbums.java
+++ app/src/main/java/com/example/craiglynagh/cool_albums/CoolAlbums.java
@@ -1,8 +1,43 @@
 package com.example.craiglynagh.cool_albums;
 
+import java.util.ArrayList;
+
 /**
  * Created by craiglynagh on 09/08/2017.
  */
 
 public class CoolAlbums {
+
+    private ArrayList<Album> list;
+
+    public CoolAlbums(){
+        list = new ArrayList<Album>();
+        list.add(new Album(1, "", 1));
+        list.add(new Album(2, "", 1));
+        list.add(new Album(3, "", 1));
+        list.add(new Album(4, "", 1));
+        list.add(new Album(5, "", 1));
+        list.add(new Album(6, "", 1));
+        list.add(new Album(7, "", 1));
+        list.add(new Album(8, "", 1));
+        list.add(new Album(9, "", 1));
+        list.add(new Album(10, "", 1));
+        list.add(new Album(11, "", 1));
+        list.add(new Album(12, "", 1));
+        list.add(new Album(13, "", 1));
+        list.add(new Album(14, "", 1));
+        list.add(new Album(15, "", 1));
+        list.add(new Album(16, "", 1));
+        list.add(new Album(17, "", 1));
+        list.add(new Album(18, "", 1));
+        list.add(new Album(19, "", 1));
+        list.add(new Album(20, "", 1));
+    }
+
+    public ArrayList<Album>getList(){
+        return new ArrayList<Album>(list);
+    }
+
 }
+
+
--- app/src/test/java/com/example/craiglynagh/cool_albums/CoolAlbumsTest.java
+++ app/src/test/java/com/example/craiglynagh/cool_albums/CoolAlbumsTest.java
@@ -1,8 +1,20 @@
 package com.example.craiglynagh.cool_albums;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by craiglynagh on 09/08/2017.
  */
 
 public class CoolAlbumsTest {
+
+    @Test
+            public void getListTest() {
+        CoolAlbums coolAlbums = new CoolAlbums();
+        assertEquals(20, coolAlbums.getList().size());
+    }
+
 }
+
